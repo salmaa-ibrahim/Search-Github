92b00fd34264ab5147a5964a24e866eb2357bd09
salmaa-ibrahim <salmaa.ibrahim19@gmail.com>
2024-08-14 13:08:59 +0300

    new

--- github user search/src/component/navbar/navbar.jsx
+++ github user search/src/component/navbar/navbar.jsx
@@ -1,7 +1,7 @@
 import { ThemeContext } from '../../App';
 import './navbar.css'
 import darkImg from '/images/Path.svg'
-
+import lightImg from '/images/002-sun (1).svg'
 import { useContext } from "react";
 
 function Navbar(){
@@ -20,12 +20,15 @@ function Navbar(){
         <div className={passevedValues.theme +"Navbar"}>
             <a href="#">devfinder</a>
             <div id="mood">
-                <div className="moodState" >
+                <div className={passevedValues.theme +"MoodState"} >
                     <input className='check' type="checkbox" onChange={onHandleChange} />
-                    <button className='moodBtn'>DARK</button>
+                    <button className={passevedValues.theme +"Dark"}>DARK</button>
+                    <button className={passevedValues.theme +"Light"}>LIGHT</button>
+
                 </div>
                 <div className="moodImg">
-                    <img src={darkImg} alt="" />
+                    <img src={darkImg} alt="" className={passevedValues.theme +"moon"} />
+                    <img src={lightImg} alt="" className={passevedValues.theme +"Sun"} />
                 </div>
             </div>
         </div>
